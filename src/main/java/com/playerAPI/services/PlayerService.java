5baea17e0fec81259c456dfb87a058bc9a16959c
juliano <julianoms00@gmail.com>
2020-06-11 21:55:23 -0300

    removing imports

--- src/main/java/com/playerAPI/services/PlayerService.java
+++ src/main/java/com/playerAPI/services/PlayerService.java
@@ -3,19 +3,9 @@ package com.playerAPI.services;
 import com.playerAPI.utils.SongGetter;
 import com.playerAPI.utils.ValidIds;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.BodyInserter;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Service
